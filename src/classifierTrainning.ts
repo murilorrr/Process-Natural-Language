55ee6049f0c28960dd731dc3869a768244c8ff84
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-27 15:30:42 -0300

    :tada: initial commit

---
+++ src/classifierTrainning.ts
@@ -0,0 +1,12 @@
+import { BayesClassifier } from 'natural'
+
+const classifier = new BayesClassifier();
+
+classifier.addDocument('i am long qqqq', 'buy');
+classifier.addDocument('buy the q\'s', 'buy');
+classifier.addDocument('short gold', 'sell');
+classifier.addDocument('sell gold', 'sell');
+classifier.train();
+
+console.log(classifier.classify('i am short silver')); // pessoa querendo vender
+console.log(classifier.classify('i am long copper')); // pessoa querendo comprar
